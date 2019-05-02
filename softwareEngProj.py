7e00a529faa99dddebafea79500fe704528662c3
Marlon Breaux <marlonbreaux24@gmail.com>
2019-05-02 11:23:32 -0500

    Update softwareEngProj.py

--- softwareEngProj.py
+++ softwareEngProj.py
@@ -10,22 +10,22 @@ listOfWords = text.split()
 print(listOfWords)
 nonrepWords = {} #creating a dictionary of non repeating words
 #if word in dictionary, increment count, if not, put in dictionary and enter 1 as count
-for word in listOfWords:
+for word in listOfWords:        
     if word not in nonrepWords:
         nonrepWords[word] = 1
     else:
         nonrepWords[word] = nonrepWords[word] + 1
 file.close() #close the file
-print(nonrepWords)
+print(nonrepWords)      #returns the values and the number of times incremented
 #for every word, 
 for word in nonrepWords:
     print(word + ', ', end='')
     print(nonrepWords[word], end='')
     print(', ', end = '')
     print(len(word), end='')
-    if word[0].isdecimal():
-        print(', ' + "digit", end='\n')
-    elif word[0].isalpha():
-        print(', ' + "alpha", end='\n')
+    if word[0].isdecimal(): #loop for decimal character variables 
+        print(', ' + "digit", end='\n') #returns every number value
+    else word[0].isalpha():
+        print(', ' + "alpha", end='\n') #returns every character or word value
     else:
-        print(', ' + "special", end='\n')
+        print(', ' + "special", end='\n')   #returns every special character value 
